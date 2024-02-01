3f6033fad11749001e60708271def52d7f721b09
fibrahim30 <fibrahim30@gatech.edu>
2024-02-01 14:41:41 -0500

    targeting i3d thumos features in yaml

--- scripts/thumos/test.sh
+++ scripts/thumos/test.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0
-python ./eval.py ./configs/temporalmaxer_thumos_videomaev2.yaml ./ckpt/thumos/bestmodel.pth.tar
+python ./eval.py ./configs/temporalmaxer_thumos_i3d.yaml ./ckpt/thumos/bestmodel.pth.tar
