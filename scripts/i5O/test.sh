cfb2206b4acbe37ffc38013020e29a23140bf2a3
fibrahim30 <fibrahim30@gatech.edu>
2024-02-01 14:16:46 -0500

    fixes

--- scripts/i5O/test.sh
+++ scripts/i5O/test.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0
-python ./eval.py ./configs/temporalmaxer_i5O_i3d.yaml ./ckpt/i5O/bestmodel.pth.tar
+python ./eval.py ./configs/temporalmaxer_i5O_videomaev2.yaml ./ckpt/i5O/bestmodel.pth.tar
