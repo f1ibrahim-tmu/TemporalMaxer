3aec962c9d0a9edd83b1f5298afdfe555db1260f
fibrahim30 <fibrahim30@gatech.edu>
2024-02-07 15:29:37 -0500

    Added i3d related files to work with extracted features

---
+++ scripts/i5O_i3d/test.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=0
+python ./eval.py ./configs/temporalmaxer_i5O_videomaev2.yaml ./ckpt/i5O/bestmodel.pth.tar
---
+++ scripts/i5O_i3d/train.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=0
+python ./train.py ./configs/temporalmaxer_i5O_videomaev2.yaml --save_ckpt_dir "./ckpt/i5O"
