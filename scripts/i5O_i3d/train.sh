1918dc1dfea38a9497c6a0df856799a43736dcdc
fibrahim30 <fibrahim30@gatech.edu>
2024-02-07 16:24:31 -0500

    Testing out FLOPs and MACs calculation

--- scripts/i5O_i3d/train.sh
+++ scripts/i5O_i3d/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0
-python ./train.py ./configs/temporalmaxer_i5O_videomaev2.yaml --save_ckpt_dir "./ckpt/i5O"
+python ./train.py ./configs/temporalmaxer_i5O_i3d_rgb.yaml --save_ckpt_dir "./ckpt/i5O"
